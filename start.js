2971e591c75f06926e6aa9e0ef03465c5665839b
Daniel- <danielyg2904@gmail.com>
2020-04-27 16:14:48 +0700

    moved messages to a new file

--- start.js
+++ start.js
@@ -5,61 +5,17 @@ var fs = require('fs');
 const pathToRegistered = './data/registered.json'
 var registered = require(pathToRegistered)
 var music = require('./modules/music.js')
+var message = require('./data/messages')
 const prefix = 'm/';
 
-const help = `
-Here's a list of things you can do:
-\`- m/help | shows you this
 
-====== Fun Area ======
-- m/peekpicture <mention> | take a peek on others 'w'
-
-====== KampungSMP Help Area ======
-- m/helpkomporpls | slimefun kompor support
-- m/helpaugmentpls | augment crafting tutorial
-
-====== Music (BETA) ======
-- m/play <keyword> | I can play a song for you if you want~
-- m/leave | just straight up leaving the voice room because you asked me to .w.
-- m/skip | skips what's playing right now
-\`
-`
-
-const refundFormat = `
-Format:
-
-1. Nickname dalam game:
-2. Waktu Kejadian:
-3. Tempat Kejadian:
-4. Kronologis Kejadian:
-5. Barang yang Hilang: 
-
-Untuk barang hilang yang memiliki enchant, silakan tulis dengan format seperti ini.
-(Untuk enchant biasa pakai underscore, untuk custom enchant tanpa spasi)
-\`\`\`
-Contoh:
-
-diamond_helmet
-aqua_affinity 1
-protection 4
-blast_protection 3
-dst.
-customenchant:
-wildmark 1
-cactus 3
-planetarydeathbringer 1
-dst.
-\`\`\`
-
-Sertakan SS bila ada, screenshot akan meningkatkan kemungkinan barangmu direfund dengan utuh, dan akan menjadi prioritas.
-`
 const embed = new MessageEmbed()
 	// Set the title of the field
 	.setTitle('**Mavis here~**')
 	// Set the color of the embed
 	.setColor(0x01a59c)
 	// Set the main content of the embed
-	.setDescription(help);
+	.setDescription(message.help);
 // Send the embed to the same channel as the message
 
 
@@ -146,8 +102,8 @@ function setDefaultVoice(guildID, voiceChannelID) {
 
 }
 client.on('channelCreate', channel => {
-	if (channel.guild.id != "617189159345586215") return;
-	if (channel.parentID != "618825049608028160") return;
+	if (channel.guild.id != "617189159345586215") return; // Kampung MC
+	if (channel.parentID != "618825049608028160") return; // Has to be on the refund channel
 	let roles = channel.guild.roles.cache;
 	let roleStaff = null;
 	let roleSpecial = null;
@@ -169,7 +125,7 @@ client.on('channelCreate', channel => {
 		for (w of warga) {
 			channel.send(`<@${w.id}>`);
 		}
-		channel.send(refundFormat);
+		channel.send(message.refundFormat);
 	}
 });
 client.on('message', message => {
