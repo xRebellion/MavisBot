abe8f89e46ff2b29ee48c1b6162a8acdf4b6212a
Daniel- <danielyg2904@gmail.com>
2021-09-21 01:19:43 +0700

    minor command changes

--- modules/cmd.js
+++ modules/cmd.js
@@ -17,7 +17,7 @@ function processCmd(message) {
             var args = message.content.substring(2).split(' ');
             var cmd = args[0];
             args = args.splice(1)
-            var commandList = ['ping', 'register', 'reactall', 'getid']
+
             if (registered.id.includes(message.author.id)) {
                 switch (cmd) {
                     // m/ping
@@ -43,11 +43,12 @@ function processCmd(message) {
                         message.channel.send('Default Voice Channel set! ~');
                         setDefaultVoice(message.guild.id, message.member.voice.channelID);
                         break;
+                    case 'getGuildList':
+                        fetchGuild(message)
+                        break;
                 }
             }
-            if (commandList.includes(cmd) && !registered.id.includes(message.author.id)) {
-                message.channel.send("You can't do that...")
-            }
+
             switch (cmd) {
                 case 'peekpicture':
                     fun.peekPicture(message, args);
@@ -63,6 +64,7 @@ function processCmd(message) {
                 case 'nowplaying':
                     music.nowPlaying(message);
                     break;
+                case 'p':
                 case 'play':
                     music.execute(message, -1);
                     break;
@@ -93,6 +95,13 @@ function processCmd(message) {
             return console.error(`Error on command ${cmd} : ${err}`)
         }
     }
+}
+
+async function fetchGuild(m) {
+    m.client.guilds.fetch().then((a) => {
+        console.log(a)
+    })
+
 }
 
 module.exports = {
